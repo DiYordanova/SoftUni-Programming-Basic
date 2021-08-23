1156148d10e487e63c69c442d22066e8b5a6d116
Dilyana <d-yordanova@mail.bg>
2021-08-24 01:24:40 +0300

    Added First Step Of Programming solution.

---
+++ 03.DepositCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int termOfDeposit = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+            double sum = deposit + termOfDeposit * ((deposit * (percentage / 100)) / 12);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 06.CharityCampaign/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _6CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            int pastryCook = int.Parse(Console.ReadLine());
+            int cake = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancake = int.Parse(Console.ReadLine());
+            double sum = ((cake * 45 + waffles * 5.8 + pancake * 3.2) * pastryCook) * day;
+            double cost = sum / 8;
+            double result = sum - cost;
+            Console.WriteLine(result);
+        }
+    }
+}
