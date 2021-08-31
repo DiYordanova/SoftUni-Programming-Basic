ee6a3320985725cd5b1d00db131706fa29e60ea2
Dilyana <d-yordanova@mail.bg>
2021-09-01 00:06:38 +0300

    Added Time +15 Minutes solution.

---
+++ 03.Conditional-Statement-Exercise/05.Time+15Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05.Time_15Minures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int timeInMinutes = hour * 60 + minutes;
+            int sumMinutes = timeInMinutes + 15;
+            int timeInHoursAfter15Min = sumMinutes / 60;
+            int timeInMinutesAfter15Min = sumMinutes % 60;
+
+            if (timeInHoursAfter15Min > 23)
+            {
+                timeInHoursAfter15Min = timeInHoursAfter15Min - 24;
+            }
+            Console.WriteLine($"{timeInHoursAfter15Min}:{timeInMinutesAfter15Min:d2}");
+        }
+    }
+}
