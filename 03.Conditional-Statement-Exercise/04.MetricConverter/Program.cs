77825326644a08437ad7bd77f447cd1eb84e6871
Dilyana <d-yordanova@mail.bg>
2021-08-31 22:58:27 +0300

    Added Metric Converter solution.

---
+++ 03.Conditional-Statement-Exercise/04.MetricConverter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04.MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string exit = Console.ReadLine();
+
+            switch (input)
+            {
+                case "m":
+                    if (exit =="cm")
+                        number = number * 100;
+                    else if (exit == "mm")
+                        number = number * 1000;
+                    break;
+                case "cm":
+                    if (exit == "m")
+                        number = number / 100;
+                    else if (exit == "mm")
+                        number = number * 10;
+                    break;
+                case "mm":
+                    if (exit == "m")
+                        number = number / 1000;
+                    else if (exit == "cm")
+                        number = number / 10;
+                    break;
+            }
+            Console.WriteLine($"{number:f3}");
+        }
+    }
+}
