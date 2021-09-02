c01c3fef46388a8420d17316676c4ab4f5fa1310
Dilyana <d-yordanova@mail.bg>
2021-09-02 20:24:50 +0300

    Added Godzila vs Kong solution.

---
+++ 03.Conditional-Statement-Exercise/06.GodzillaVsKong/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            int countpeople = int.Parse(Console.ReadLine());
+            double priceClothesPerson = double.Parse(Console.ReadLine());
+
+            double priceClothesPeople = priceClothesPerson * countpeople;
+            double movieSet = budjet * 0.10;
+
+            if (countpeople > 150)
+            {
+                priceClothesPeople -= priceClothesPeople * 0.1;
+            }
+            double costs = priceClothesPeople + movieSet;
+            
+            if (costs > budjet)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {costs - budjet:f2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {budjet - costs:f2} leva left.");
+            }
+
+        }
+    }
+}
+
