7d718b0daa25b96d7a0654eeaa548faebbe704fa
Dilyana <d-yordanova@mail.bg>
2021-09-14 19:41:55 +0300

    Added solution.

---
+++ 04.Conditional-Statement-Advance/04.PersonalTitles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05.PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string jender = Console.ReadLine();
+
+            if (age >= 16 && jender == "m")
+            {
+                Console.WriteLine("Mr.");
+            }
+            else if (age < 16 && jender == "m")
+            {
+                Console.WriteLine("Master");
+            }
+            else if (age >= 16 && jender == "f")
+            {
+                Console.WriteLine("Ms.");
+            }
+            else if (age < 16 && jender == "f")
+            { 
+                Console.WriteLine("Miss");
+            }           
+        }
+    }
+}
