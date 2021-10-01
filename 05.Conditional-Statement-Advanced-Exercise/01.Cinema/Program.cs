dc637e0b54758d5c18f19bfd1792ca48005892b5
Dilyana <d-yordanova@mail.bg>
2021-10-02 00:54:41 +0300

    Added solution.

--- 05.Conditional-Statement-Advanced-Exercise/01.Cinema/Program.cs
+++ 05.Conditional-Statement-Advanced-Exercise/01.Cinema/Program.cs
@@ -6,7 +6,24 @@ namespace _01.Cinema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string movie = Console.ReadLine();
+            int row = int.Parse(Console.ReadLine());
+            int column = int.Parse(Console.ReadLine());
+
+            double income = 0;
+            if (movie == "Premiere")
+            {
+                income = row * column * 12;
+            }
+            else if (movie == "Normal")
+            {
+                income = row * column * 7.50;
+            }
+            else if (movie == "Discount")
+            {
+                income = row * column * 5;
+            }
+            Console.WriteLine($"{income:f2} leva");
         }
     }
 }
