70e8f06ffe766e11a698156fac237bd974cf79b7
Dilyana <d-yordanova@mail.bg>
2021-10-08 17:51:10 +0300

    Added solution.

---
+++ 05.Conditional-Statement-Advanced-Exercise/05.Journey/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _05.Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            string destination = "";
+            double spendSum = 0;
+
+            if (budjet <= 100)
+            {
+                destination = "Bulgaria";
+                switch (season)
+                {
+                    case "summer":
+                        spendSum = 0.3 * budjet;
+                        break;
+                    case "winter":
+                        spendSum = 0.7 * budjet;
+                        break;
+                }
+            }
+            else if (budjet > 101 && budjet <= 1000)
+            {
+                destination = "Balkans";
+                switch (season)
+                {
+                    case "summer":
+                        spendSum = 0.4 * budjet;
+                        break;
+                    case "winter":
+                        spendSum = 0.8 * budjet;
+                        break;
+                }
+            }
+            else if (budjet > 1000)
+            {
+                destination = "Europe";
+                spendSum = 0.9 * budjet;
+            }
+
+            string typeOfRest = "";
+
+            if (destination == "Europe")
+            {
+                typeOfRest = "Hotel";
+            }
+            else if (season == "summer")
+            {
+                typeOfRest = "Camp";
+            }
+            else if (season == "winter")
+            {
+                typeOfRest = "Hotel";
+            }
+
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{typeOfRest} - {spendSum:f2}");
+        }
+    }
+}
