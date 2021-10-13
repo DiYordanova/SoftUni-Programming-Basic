fd5d84f18315317e636c0875fa044542787acf83
Dilyana <d-yordanova@mail.bg>
2021-10-13 21:54:46 +0300

    Added solution.

---
+++ 05.Conditional-Statement-Advanced-Exercise/09.Volleyball/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _09.Volleyball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string year = Console.ReadLine();
+            double holiday = int.Parse(Console.ReadLine());
+            int weekendInBirthPlace = int.Parse(Console.ReadLine());
+
+            int weekendInSofia = 48 - weekendInBirthPlace;
+            double weekendInSofiaPlay = 0.75 * weekendInSofia;
+            double holidayPlay = (holiday * 2) / 3;
+            double countPlay = weekendInSofiaPlay + weekendInBirthPlace + holidayPlay;
+
+            if (year == "leap")
+            {
+                countPlay += countPlay * 0.15;
+            }
+            Console.WriteLine(Math.Floor(countPlay));
+        }
+    }
+}
