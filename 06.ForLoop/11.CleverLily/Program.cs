16ebb406f45a334e3e235f77a45d67e87573d8d0
Dilyana <d-yordanova@mail.bg>
2021-10-27 12:09:19 +0300

    Added solution.

---
+++ 06.ForLoop/11.CleverLily/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _11.CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            double toyPrace = double.Parse(Console.ReadLine());
+
+            int safeMoney = 0;
+            int presentMoney = 10;
+            int toyCount = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    safeMoney = presentMoney + safeMoney;
+                    presentMoney += 10;
+                    safeMoney = safeMoney - 1;
+                }
+                else
+                {
+                    toyCount++;
+                }
+            }
+            double totalToysPrice = toyCount * toyPrace;
+            double incom = totalToysPrice + safeMoney;
+
+            if (incom >= washingMachinePrice)
+            {
+                Console.WriteLine($"Yes! {incom - washingMachinePrice:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washingMachinePrice - incom:f2}");
+            }
+        }
+    
+    }
+}
