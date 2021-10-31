c8d1e29edab81e634799f484e0b947707cf42e32
Dilyana <d-yordanova@mail.bg>
2021-10-31 10:07:13 +0200

    Added solution.

---
+++ 07.ForLoopExercise/05.DivideWithoutRemainder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04.Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double count = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            for (int i = 0; i < count; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+                if (current % 2 == 0)
+                {
+                    p1++;
+                }
+
+                if (current % 3 == 0)
+                {
+                    p2++;
+                }
+
+                if (current % 4 == 0)
+                {
+                    p3++;
+                }
+
+            }
+
+            Console.WriteLine($"{p1 / count * 100:f2}%");
+            Console.WriteLine($"{p2 / count * 100:f2}%");
+            Console.WriteLine($"{p3 / count * 100:f2}%");
+        }
+    }    
+}
+    
+
