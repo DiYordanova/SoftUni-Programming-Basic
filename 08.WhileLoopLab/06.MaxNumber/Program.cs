cf7d3f522b22bb772411c5fce930334450844ef9
Dilyana <d-yordanova@mail.bg>
2021-11-04 23:51:59 +0200

    Added solution.

---
+++ 08.WhileLoopLab/06.MaxNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int max = int.MinValue;
+            while (input!="Stop")
+            {
+                int amount = int.Parse(input);
+                if (amount > max)
+                {
+                    max = amount;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
