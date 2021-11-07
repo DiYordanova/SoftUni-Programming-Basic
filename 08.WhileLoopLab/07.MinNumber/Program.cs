a5e16123e6e03df9d762f390b0ca4fdafff85a68
Dilyana <d-yordanova@mail.bg>
2021-11-07 10:24:28 +0200

    Added solution.

---
+++ 08.WhileLoopLab/07.MinNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07.MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int min = int.MaxValue;
+            while (input != "Stop")
+            {
+                int amount = int.Parse(input);
+                if (amount < min)
+                {
+                    min = amount;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
