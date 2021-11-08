e7489f64e2dab24b8353247a2c9fe0e75680f9b9
Dilyana <d-yordanova@mail.bg>
2021-11-08 15:04:07 +0200

    Added solution.

---
+++ 08.WhileLoopLab/09.Mooving/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _09.Mooving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int freeSpaceVolume = width * lenght * height;
+            int spaceTaken = 0;
+            string input = Console.ReadLine(); ;
+            while (input != "Done")
+            {
+                int inputInInt = int.Parse(input);      //
+                spaceTaken += inputInInt;              // spaceTaken += int.Parse(input)
+                if (spaceTaken >= freeSpaceVolume)
+                {
+                    Console.WriteLine($"No more free space! You need {spaceTaken - freeSpaceVolume} Cubic meters more."
+);
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "Done")
+            {
+                Console.WriteLine($"{freeSpaceVolume - spaceTaken} Cubic meters left.");
+            }
+        }
+    }
+}
