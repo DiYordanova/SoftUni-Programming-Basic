8bcaadaf91013ef67e4414e679f4dcaa6a3f2afd
Dilyana <d-yordanova@mail.bg>
2021-11-09 10:00:21 +0200

    Added solutions.

---
+++ 09.WhileLoopExercise/01.Library/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WhileLoopExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favoriteBook = Console.ReadLine();
+            string nextBook = Console.ReadLine();
+            int counter = 0;
+            while (nextBook != favoriteBook && nextBook != "No More Books")
+            {
+                nextBook = Console.ReadLine();
+                counter++;
+            }
+
+            if (nextBook == favoriteBook)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+            }
+        }
+    }
+}
---
+++ 09.WhileLoopExercise/02.ExamPreparation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _02.ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int poorGrade = int.Parse(Console.ReadLine());
+            int counter =0;
+            int counter2 = 0;
+            double sumGrade = 0;            
+            string lastName = "";
+            bool isEnough = false;
+            while (!isEnough && counter2 != poorGrade)
+            {
+                string nameGrade = Console.ReadLine();
+                if (nameGrade == "Enough")
+                {
+                    isEnough = true;
+                     break;
+                }
+
+                lastName = nameGrade;
+                counter++;
+                int grade = int.Parse(Console.ReadLine());
+                sumGrade += grade;
+                if (grade <= 4)
+                {
+                    counter2++;
+                }
+
+            }
+
+            if (isEnough)
+            {
+                Console.WriteLine($"Average score: {sumGrade / counter:f2}");
+                Console.WriteLine($"Number of problems: {counter}");
+                Console.WriteLine($"Last problem: {lastName}");
+            }
+
+            else
+            {
+                Console.WriteLine($"You need a break, {counter2} poor grades.");
+            }
+        }
+    }
+}
