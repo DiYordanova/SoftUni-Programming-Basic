80f811d44b376f48377fb3b4558226fe69a6f193
Dilyana <d-yordanova@mail.bg>
2021-11-09 11:12:47 +0200

    Added solution.

---
+++ 09.WhileLoopExercise/05.Coins/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _05.Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal input = decimal.Parse(Console.ReadLine());
+            decimal change = input * 100;
+            int coins = 0;
+            while (change > 0)
+            {
+                coins++;
+                if (change >= 200)
+                {
+                    change -= 200;
+                }
+                else if (change >= 100)
+                {
+                    change -= 100;
+                }
+                else if (change >= 50)
+                {
+                    change -= 50;
+                }
+                else if (change >= 20)
+                {
+                    change -= 20;
+                }
+                else if (change >= 10)
+                {
+                    change -= 10;
+                }
+                else if (change >= 5)
+                {
+                    change -= 5;
+                }
+                else if (change >= 2)
+                {
+                    change -= 2;
+                }
+                else if (change >= 1)
+                {
+                    change -= 1;
+                }
+            }
+            Console.WriteLine(coins);
+        }
+    }
+}
