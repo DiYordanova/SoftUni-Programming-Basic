d8bb7697e4f3b162c04f8bc88534626c11b06f13
Dilyana <d-yordanova@mail.bg>
2021-11-11 08:30:57 +0200

    Added solution.

---
+++ 09.WhileLoopExercise/06.Cake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int sizeCake = width * lenght;
+            int sumCake = 0;
+            string input = Console.ReadLine();
+            bool isEnought = false;
+            while (!isEnought && input != "STOP")
+            {
+                int pieceCake = int.Parse(input);
+                sumCake += pieceCake;
+                if (sizeCake <= sumCake)
+                {
+                    isEnought = true;
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (isEnought)
+            {
+                Console.WriteLine($"No more cake left! You need {sumCake - sizeCake} pieces more.");
+            }
+            else
+            {
+                Console.WriteLine($"{sizeCake - sumCake} pieces are left.");
+            }
+        }
+    }
+}
