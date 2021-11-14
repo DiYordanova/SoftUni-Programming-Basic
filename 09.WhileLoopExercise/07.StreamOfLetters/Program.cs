9b853b2bc4909d760ce0e9a07a369d6aca72c6a0
Dilyana <d-yordanova@mail.bg>
2021-11-14 08:46:01 +0200

    Added solution.

---
+++ 09.WhileLoopExercise/07.StreamOfLetters/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace MoreExerciseWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            bool hasC = false;
+            bool hasO = false;
+            bool hasN = false;
+            string word = "";
+            while (input != "End")
+            {
+                if (input == "c")
+                {
+                    if (hasC)
+                    {
+                        word += input;
+                    }
+                    else
+                    {
+                        hasC = true;
+                    }
+                }
+
+                else if (input == "o")
+                {
+                    if (hasO)
+                    {
+                        word += input;
+                    }
+                    else
+                    {
+                        hasO = true;
+                    }
+
+                }
+
+                else if (input == "n")
+                {
+                    if (hasN)
+                    {
+                        word += input;
+                    }
+                    else
+                    {
+                        hasN = true;
+                    }
+
+                }
+
+                else
+                {
+                    int charNum = input[0];
+                    if ((65 <= charNum && charNum <= 90) || (97 <= charNum && charNum <= 122))
+                    {
+                        word += input;
+                    }
+                }
+                if (hasC && hasO && hasN)
+                {
+                    Console.Write($"{word} ");
+                    word = "";
+                    hasC = false;
+                    hasO = false;
+                    hasN = false;
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
+
