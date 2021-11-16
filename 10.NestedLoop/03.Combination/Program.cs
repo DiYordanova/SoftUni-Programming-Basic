e6de42fc42cb88a3b16af2260df86b5cbace977f
Dilyana <d-yordanova@mail.bg>
2021-11-16 15:57:20 +0200

    Added solution.

---
+++ 10.NestedLoop/03.Combination/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03.Combination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int number = int.Parse(Console.ReadLine());
+            for (int x1 = 0; x1 <= number; x1++)
+            {
+                for (int x2 = 0; x2 <= number; x2++)
+                {
+                    for (int x3 = 0; x3 <= number; x3++)
+                    {
+                        int sum = 0;
+                        sum = x1 + x2 + x3;
+                        if (sum == number)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+
+
+}
