5e3ab28b14a8700fd7a8f1f50f97f6702e5857ff
Dilyana <d-yordanova@mail.bg>
2021-11-19 12:52:35 +0200

    Added solution.

---
+++ 10.NestedLoop/05.Travelling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            while (destination != "End")
+            {
+                double budjet = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+
+                while (savedMoney < budjet)
+                {
+                    double currenSum = double.Parse(Console.ReadLine());
+                    savedMoney += currenSum;
+                }
+                Console.WriteLine($"Going to {destination}!");                
+                destination = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
---
+++ 10.NestedLoop/06.Building/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floоr = int.Parse(Console.ReadLine());
+            int flat = int.Parse(Console.ReadLine());
+            for (int fr = floоr; fr >= 1; fr--)
+            {
+                for (int ft = 0; ft < flat; ft++)
+                {
+                    if (fr == floоr)
+                    {
+                        Console.Write($"L{fr}{ft} ");                    
+                    }
+                    
+                    if (fr %2 == 0 && fr != floоr)
+                    {
+                        Console.Write($"O{fr}{ft} ");
+                    }
+
+                    if (fr % 2 != 0 && fr != floоr)
+                    {
+                        Console.Write($"A{fr}{ft} ");
+                    }                 
+                }
+                Console.WriteLine();
+            }    
+        }
+    }
+}             
+        
+    
+
