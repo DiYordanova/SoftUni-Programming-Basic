b52b93959e66b01a48ab4ac3c62a70c89cda9cb3
Dilyana <d-yordanova@mail.bg>
2021-11-20 18:29:45 +0200

    Added solution.

---
+++ 11.NastedLoopsExcersise/02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02._Equal_Sums_Even_Odd_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+            
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                string numAsText = i.ToString();
+                    int oddSum = 0;
+                    int evenSum = 0;
+                for (int index = 0; index < numAsText.Length; index++)
+                {
+                    int num = int.Parse(numAsText[index].ToString());
+                    if (index % 2 == 0)
+                    {
+                        evenSum += num;
+                    }
+                    else
+                    {
+                        oddSum += num;
+                    }
+                }
+                if (oddSum == evenSum)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
