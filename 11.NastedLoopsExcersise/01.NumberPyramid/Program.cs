53e2f4874912be5b34d0074ef5a3d1cc9b685ba7
Dilyana <d-yordanova@mail.bg>
2021-11-20 18:34:37 +0200

    Added solution.

---
+++ 11.NastedLoopsExcersise/01.NumberPyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NastedLoopsExcersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int current = 1;
+            bool isBigger = false;
+            for (int row = 1; row <= number; row++)
+            {
+                for (int col = 1; col <= row; col++)
+                {
+                    if (current > number)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(current + " ");
+                    current++;
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
