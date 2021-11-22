0f500468f962c4aed75e39c56fc2f4ad45e6f291
Dilyana <d-yordanova@mail.bg>
2021-11-22 13:00:23 +0200

    Added solution.

---
+++ 11.NastedLoopsExcersise/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04.TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryCount = int.Parse(Console.ReadLine());
+            string namePreсentaion = Console.ReadLine();
+            double sumGrade = 0;
+            int counter = 0;
+            double totalSumGrade = 0;        
+            while (namePreсentaion != "Finish")
+            {                
+                sumGrade = 0;
+                for (int i = 0; i < juryCount; i++)
+                {
+                double grade = double.Parse(Console.ReadLine());
+                sumGrade += grade;
+                counter++;                
+                }
+
+                Console.WriteLine($"{namePreсentaion} - {(sumGrade / juryCount):f2}.");
+                namePreсentaion = Console.ReadLine();
+                totalSumGrade += sumGrade;
+
+            }
+            if (namePreсentaion == "Finish")
+            {
+                Console.WriteLine($"Student's final assessment is {(totalSumGrade / counter):f2}.");
+            }
+        }
+    }
+}
