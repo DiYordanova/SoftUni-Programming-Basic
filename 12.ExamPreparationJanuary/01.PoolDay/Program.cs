5519b553923ef6fe1068039a5889476fcd7700f9
Dilyana <d-yordanova@mail.bg>
2021-11-24 17:29:51 +0200

    Added solution.

---
+++ 12.ExamPreparationJanuary/01.PoolDay/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExamPreparationJanuary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPeople = int.Parse(Console.ReadLine());
+            double entranceFee = double.Parse(Console.ReadLine());
+            double sunbedPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+            double totalEntranceFee = entranceFee * numOfPeople;
+            double totalSunbedPrice = Math.Ceiling(0.75 * numOfPeople) * sunbedPrice;
+            double totalUmbrellaPrice = Math.Ceiling(numOfPeople * 0.5) * umbrellaPrice;
+            Console.WriteLine($"{totalEntranceFee + totalSunbedPrice + totalUmbrellaPrice:f2} lv.");
+        }
+    }
+}
---
+++ 12.ExamPreparationJanuary/02.Shopping/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            int videoCard = int.Parse(Console.ReadLine());
+            int processor = int.Parse(Console.ReadLine());
+            int ramMemory = int.Parse(Console.ReadLine());
+            double videoCardPrice = 250;
+            double videoCardTotalPrice = videoCard * videoCardPrice;
+            double processorPrice = videoCardTotalPrice * 0.35;
+            double ramMemoryPrice = videoCardTotalPrice * 0.10;
+            double totalprice = videoCardTotalPrice + processor * processorPrice + ramMemory * ramMemoryPrice;
+            if (videoCard > processor)
+            {
+                totalprice = totalprice - totalprice * 0.15;
+            }
+
+            if (budjet >= totalprice)
+            {
+                Console.WriteLine($"You have {budjet - totalprice:f2} leva left!");
+            }
+
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {totalprice - budjet:f2} leva more!");
+            }
+        }
+    }
+}
