a18c02df9f058edca3f1d2f2d4b771e48d6720d1
Dilyana <d-yordanova@mail.bg>
2021-11-26 08:23:36 +0200

    Added course solution.

---
+++ 12.ExamPreparationJanuary/06.EasterCompetition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int easterBread = int.Parse(Console.ReadLine());
+            int maxPoints = int.MinValue;
+            string bakerNumber1 = "";
+            for (int i = 0; i < easterBread; i++)
+            {
+                string baker = Console.ReadLine();
+                int totalPoints = 0;
+                string command = Console.ReadLine();
+                while (command != "Stop")
+                {
+                    int points = int.Parse(command);
+                    totalPoints += points;
+                    command = Console.ReadLine();
+
+                }
+
+                Console.WriteLine($"{baker} has {totalPoints} points.");
+
+                if (totalPoints > maxPoints)
+                {
+                    maxPoints = totalPoints;
+                    bakerNumber1 = baker;
+                    Console.WriteLine($"{baker} is the new number 1!");
+                }
+
+            }
+            Console.WriteLine($"{bakerNumber1} won competition with {maxPoints} points!");
+        }
+    }
+}
+
