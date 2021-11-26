4ee2d9c101849ec93da0960a1d9d991da958619d
Dilyana <d-yordanova@mail.bg>
2021-11-26 08:17:17 +0200

    Added course solution.

---
+++ 12.ExamPreparationJanuary/05.Club/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double target = double.Parse(Console.ReadLine());
+            string cocktail = Console.ReadLine();
+            double costTotalCocktail = 0;
+            double income = 0;
+            while (cocktail != "Party!")
+            {
+                int numbercocktail = int.Parse(Console.ReadLine());
+                int costForOneCocktail = cocktail.Length;
+                costTotalCocktail = numbercocktail * costForOneCocktail;
+                if (costTotalCocktail % 2 == 1)
+                {
+                    costTotalCocktail = costTotalCocktail * 0.75;
+                }
+                income += costTotalCocktail;
+                if (income >= target)
+                {
+                    Console.WriteLine("Target acquired.");
+                    Console.WriteLine($"Club income - {income:f2} leva.");
+                    break;
+                }
+
+                cocktail = Console.ReadLine();
+            }
+
+            if (cocktail == "Party!")
+            {
+                Console.WriteLine($"We need {target - income:f2} leva more.");
+                Console.WriteLine($"Club income - {income:f2} leva.");
+            }
+        }
+    }
+}
+
