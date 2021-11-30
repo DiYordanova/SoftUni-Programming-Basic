e1d2cfa64b39514efd390dd00eb74dc08f0ff866
Dilyana <d-yordanova@mail.bg>
2021-12-01 00:20:19 +0200

    Added solution.

---
+++ 13.ExamPreparationFebruary/05.HighJump/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int desiredHeight = int.Parse(Console.ReadLine());
+            int currentDesiredHeight = desiredHeight - 30;
+            int counter = 0;
+            int counterUnsuccessful = 0;
+            bool isSuxccess = false;
+            while (!isSuxccess)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    int currentHeight = int.Parse(Console.ReadLine());
+                    counter++;
+                    if (currentHeight > desiredHeight)
+                    {
+                        isSuxccess = true;
+                        break;
+                    }
+
+                    else if (currentHeight > currentDesiredHeight)
+                    {
+
+                        break;
+                    }
+
+                    else if (counterUnsuccessful < 3)
+                    {
+                        counterUnsuccessful++;
+
+                    }
+
+                    if (counterUnsuccessful == 3)
+                    {
+                        Console.WriteLine($"Tihomir failed at {currentDesiredHeight}cm after {counter} jumps.");
+                        return;
+                    }
+                }
+                counterUnsuccessful = 0;
+
+
+                currentDesiredHeight += 5;
+            }
+
+            if (isSuxccess)
+            {
+                Console.WriteLine($"Tihomir succeeded, he jumped over {desiredHeight}cm after {counter} jumps.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Tihomir failed at {currentDesiredHeight}cm after {counter} jumps.");
+            }
+        }
+    }
+}
+
