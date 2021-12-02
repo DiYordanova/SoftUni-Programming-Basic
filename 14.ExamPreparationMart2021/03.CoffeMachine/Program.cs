989f7546a98b35e36c30c4dd07f74d7b2a2beddf
Dilyana <d-yordanova@mail.bg>
2021-12-02 23:24:27 +0200

    Added solution.

---
+++ 14.ExamPreparationMart2021/03.CoffeMachine/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace _03.CoffeMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string drink = Console.ReadLine();
+            string shugar = Console.ReadLine();
+            int countDrink = int.Parse(Console.ReadLine());
+            double price = 0;
+            switch (drink)
+            {
+                case "Espresso":
+                    if (shugar == "Without")
+                    {
+                        price = 0.90;
+                    }
+
+                    else if (shugar == "Normal")
+                    {
+                        price = 1.00;
+                    }
+
+                    else if (shugar == "Extra")
+                    {
+                        price = 1.20;
+                    }
+                    break;
+                case "Cappuccino":
+                    if (shugar == "Without")
+                    {
+                        price = 1.00;
+                    }
+
+                    else if (shugar == "Normal")
+                    {
+                        price = 1.20;
+                    }
+
+                    else if (shugar == "Extra")
+                    {
+                        price = 1.60;
+                    }
+                    break;
+                case "Tea":
+                    if (shugar == "Without")
+                    {
+                        price = 0.50;
+                    }
+
+                    else if (shugar == "Normal")
+                    {
+                        price = 0.60;
+                    }
+
+                    else if (shugar == "Extra")
+                    {
+                        price = 0.70;
+                    }
+                    break;
+            }
+
+            if (shugar == "Without")
+            {
+                price = price - price * 0.35;
+            }
+
+            if (drink == "Espresso" && countDrink >= 5)
+            {
+                price = price - price * 0.25;
+            }
+
+            price = price * countDrink;
+            if (price > 15.00)
+            {
+                price = price - price * 0.20;
+            }
+
+            Console.WriteLine($"You bought {countDrink} cups of {drink} for {price:f2} lv.");
+        }
+    }
+}
+ 
---
+++ 14.ExamPreparationMart2021/04.TrekkingMania/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _04.TrekkingMania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countGroup = int.Parse(Console.ReadLine());
+            int allPeople = 0;
+            int countMusala = 0;
+            int countMonblan = 0;
+            int countKilimandjaro = 0;
+            int countK2 = 0;
+            int countEverest = 0;
+            for (int i = 0; i < countGroup; i++)
+            {
+                int countPeopleInOneGroup = int.Parse(Console.ReadLine());
+
+                if (countPeopleInOneGroup <= 5)
+                {
+                    countMusala += countPeopleInOneGroup;
+
+                }
+
+                else if (countPeopleInOneGroup <= 12)
+                {
+                    countMonblan += countPeopleInOneGroup;
+
+                }
+
+                else if (countPeopleInOneGroup <= 25)
+                {
+                    countKilimandjaro += countPeopleInOneGroup;
+
+                }
+
+                else if (countPeopleInOneGroup <= 40)
+                {
+                    countK2 += countPeopleInOneGroup;
+
+                }
+
+                else if (countPeopleInOneGroup >= 41)
+                {
+                    countEverest += countPeopleInOneGroup;
+                }
+
+                allPeople += countPeopleInOneGroup;
+
+            }
+            double percentageMusala = (double)countMusala / allPeople * 100;
+            double percentageMonblan = (double)countMonblan / allPeople * 100;
+            double percentageKilimandjaro = (double)countKilimandjaro / allPeople * 100;
+            double percentageK2 = (double)countK2 / allPeople * 100;
+            double percentageEverest = (double)countEverest / allPeople * 100;
+            Console.WriteLine($"{percentageMusala:f2}%");
+            Console.WriteLine($"{percentageMonblan:f2}%");
+            Console.WriteLine($"{percentageKilimandjaro:f2}%");
+            Console.WriteLine($"{percentageK2:f2}%");
+            Console.WriteLine($"{percentageEverest:f2}%");
+        }
+    }
+}
