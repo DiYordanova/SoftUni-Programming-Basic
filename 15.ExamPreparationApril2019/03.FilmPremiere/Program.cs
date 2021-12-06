adcf8479e1044af2a52e38773217a74e85ccb920
Dilyana <d-yordanova@mail.bg>
2021-12-06 20:17:38 +0200

    Added solution.

---
+++ 15.ExamPreparationApril2019/03.FilmPremiere/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _03.FilmPremiere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            string type = Console.ReadLine();
+            int ticket = int.Parse(Console.ReadLine());
+            double price = 0;
+            switch (type)
+            {
+                case "Drink":
+                    if (movie == "John Wick")
+                    {
+                        price = 12;
+                    }
+
+                    else if (movie == "Star Wars")
+                    {
+                        price =18;
+                    }
+
+                    else if (movie == "Jumanji")
+                    {
+                        price = 9;
+                    }
+                    break;
+                case "Popcorn":
+                    if (movie == "John Wick")
+                    {
+                        price = 15;
+                    }
+
+                    else if (movie == "Star Wars")
+                    {
+                        price = 25;
+                    }
+
+                    else if (movie == "Jumanji")
+                    {
+                        price = 11;
+                    }
+                    break;
+                case "Menu":
+                    if (movie == "John Wick")
+                    {
+                        price = 19;
+                    }
+
+                    else if (movie == "Star Wars")
+                    {
+                        price = 30;
+                    }
+
+                    else if (movie == "Jumanji")
+                    {
+                        price = 14;
+                    }
+                    break;
+            }
+
+            if (movie == "Star Wars" && ticket >= 4 )
+            {
+                price = price - price * 0.3;
+            }
+
+            if (movie == "Jumanji" && ticket == 2)
+            {
+                price = price - price * 0.15;
+            }
+
+            Console.WriteLine($"Your bill is {ticket * price:f2} leva.");
+        }
+    }
+}
+       
