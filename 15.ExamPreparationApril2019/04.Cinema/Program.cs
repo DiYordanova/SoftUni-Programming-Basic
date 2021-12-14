b1ada8978a468a0312aa3121ca2861c67a145e92
Dilyana <d-yordanova@mail.bg>
2021-12-14 09:08:06 +0200

    Added solution.

---
+++ 15.ExamPreparationApril2019/04.Cinema/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04.Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = int.Parse(Console.ReadLine());
+            string people = Console.ReadLine();
+            int totalInkome = 0;
+            
+            while (people != "Movie time!")
+            {
+                int income = 0;
+                int peopleNumber = int.Parse(people);
+
+                if (peopleNumber > capacity)
+                {
+                    Console.WriteLine($"The cinema is full.");
+                    Console.WriteLine($"Cinema income - {totalInkome} lv.");
+                    return;
+                }
+                capacity -= peopleNumber;
+                income = peopleNumber * 5;
+                if (peopleNumber % 3 ==0)
+                {
+                    income = income - 5;
+                }
+                totalInkome += income;
+                people = Console.ReadLine();
+            }
+
+            if (people == "Movie time!")
+            {
+                
+                Console.WriteLine($"There are {capacity} seats left in the cinema.");
+                Console.WriteLine($"Cinema income - {totalInkome} lv.");
+            }
+        }
+    }
+}
