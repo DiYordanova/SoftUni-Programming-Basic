84f21c45efc1c2d913f8f2ecbbc861e0473105ef
Dilyana <d-yordanova@mail.bg>
2021-12-18 12:39:28 +0200

    Added solution.

---
+++ 15.ExamPreparationApril2019/04.Renovation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _04.Renovation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double areaNotPaint = double.Parse(Console.ReadLine());
+            double totalArea = height * width * 4;
+            double meterForPaint = (int)Math.Ceiling(totalArea - totalArea * ((double)areaNotPaint / 100));
+            string literPaint = Console.ReadLine();
+            while (literPaint != "Tired!")
+            {
+                double literPaintAsNumber = double.Parse(literPaint);
+                meterForPaint -= literPaintAsNumber;
+                if (meterForPaint < 0)
+                {
+                    Console.WriteLine($"All walls are painted and you have {Math.Abs(meterForPaint)} l paint left!" );
+                    return;
+                }
+
+                else if (meterForPaint == 0)
+                {
+                    Console.WriteLine($"All walls are painted! Great job, Pesho!");
+                    return;
+                }
+
+                literPaint = Console.ReadLine();
+            }
+
+            if (literPaint == "Tired!")
+            {
+                Console.WriteLine($"{meterForPaint} quadratic m left.");
+            }
+        }
+    }
+}
