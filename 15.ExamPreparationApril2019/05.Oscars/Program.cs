4fe3412e6cfbec6dc556a05eda26dc4db6e9f930
Dilyana <d-yordanova@mail.bg>
2021-12-22 15:40:48 +0200

    Added solution.

---
+++ 15.ExamPreparationApril2019/05.Oscars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _05.Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameActor = Console.ReadLine();
+            double pointEnter = double.Parse(Console.ReadLine());
+            int juryNumber = int.Parse(Console.ReadLine());
+            for (int i = 0; i < juryNumber; i++)
+            {
+                string nameJury = Console.ReadLine();
+                double pointJury = double.Parse(Console.ReadLine());
+                pointEnter += (nameJury.Length * pointJury) / 2;
+                if (pointEnter > 1250.5)
+                {
+                    Console.WriteLine($"Congratulations, {nameActor} got a nominee for leading role with {pointEnter:f1}!");
+                    return;
+                }               
+            }
+            Console.WriteLine($"Sorry, {nameActor} you need {1250.5 - pointEnter:f1} more!");
+        }
+    }
+}
