d33c0384f0f17121f05f148804ab6ce8e0beeed3
Dilyana <d-yordanova@mail.bg>
2021-12-22 10:48:19 +0200

    Added solution.

---
+++ 15.ExamPreparationApril2019/05.FootballTournament/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _05.FootballTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int game = int.Parse(Console.ReadLine());
+            int w = 0;
+            int d = 0;
+            int l = 0;
+            for (int i = 0; i < game; i++)
+            {
+                string inputRezult = Console.ReadLine();
+                if (inputRezult == "W")
+                {
+                    w++;
+                }
+
+                else if (inputRezult == "D")
+                {
+                    d++;
+                }
+
+                else if (inputRezult == "L")
+                {
+                    l++;
+                }
+            }
+
+            double point = w * 3 + d;
+            double winGame = (double)w / game * 100;
+            if (game == 0)
+            {
+                Console.WriteLine($"{name} hasn't played any games during this season.");
+            }
+
+            else
+            {
+            Console.WriteLine($"{name} has won {point} points during this season.");
+            Console.WriteLine($"Total stats:");
+            Console.WriteLine($"## W: {w}");
+            Console.WriteLine($"## D: {d}");
+            Console.WriteLine($"## L: {l}");
+            Console.WriteLine($"Win rate: {winGame:f2}%");          
+            }
+        }
+    }
+}
