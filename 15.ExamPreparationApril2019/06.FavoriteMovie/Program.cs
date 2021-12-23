77d3e5106978f3c8d20e73c4c633f39e6f5c810f
Dilyana <d-yordanova@mail.bg>
2021-12-23 14:20:01 +0200

    Added solution.

---
+++ 15.ExamPreparationApril2019/06.FavoriteMovie/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _06.FavoriteMovie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string nameMax = "";
+            int numberFilm = 0;
+            int maxSymbol = int.MinValue;
+            while (name != "STOP")
+            {
+                int sumSymbol = 0;
+                numberFilm++;              
+                for (int i = 0; i < name.Length; i++)
+                {
+                  char symbol = name[i];
+                   
+                    if (symbol >= 65 && symbol <= 90)
+                    {
+                        sumSymbol = sumSymbol - name.Length;
+                    }
+
+                    else if (symbol >= 97 && symbol <= 122)
+                    {
+                        sumSymbol = sumSymbol - 2 * (name.Length);
+                    }
+                    sumSymbol += symbol;
+                }
+
+                if (sumSymbol > maxSymbol)
+                {
+                    maxSymbol = sumSymbol;
+                    nameMax = name;
+                }
+
+                if (numberFilm == 7)
+                {
+                    Console.WriteLine($"The limit is reached.");
+                    break;
+                }
+                name = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The best movie for you is {nameMax} with {maxSymbol} ASCII sum.");
+        }
+    }
+}
