250d9a86f095c4b7f9ab1a04db4dad3dc207f9a9
Dilyana <d-yordanova@mail.bg>
2021-12-24 08:45:01 +0200

    Added solution.

---
+++ 15.ExamPreparationApril2019/06.TheMostPowerfulWord/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _06.TheMostPowerfulWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double maxValueWord = double.MinValue;
+            string winWord = "";
+            while (input != "End of words")
+            {
+                double valueWord = 0;
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char symbol = input[i];
+                    valueWord += symbol;
+                }
+                    if (input[0] == 'a' || input[0] == 'e' || input[0] == 'i' || input[0] == 'o' ||
+                        input[0] == 'u' || input[0] == 'y' || input[0] == 'A' || input[0] == 'E' ||
+                        input[0] == 'I' || input[0] == 'O' || input[0] == 'U' || input[0] == 'Y')
+                    {
+                        valueWord = valueWord * input.Length;
+                    }
+                    else
+                    {
+                        valueWord = Math.Floor(valueWord / input.Length);
+                    }
+
+                if (valueWord > maxValueWord)
+                {
+                    maxValueWord = valueWord;
+                    winWord = input;
+                }
+                input = Console.ReadLine();
+            }
+                Console.WriteLine($"The most powerful word is {winWord} - {maxValueWord}");
+        }
+    }
+}
+
