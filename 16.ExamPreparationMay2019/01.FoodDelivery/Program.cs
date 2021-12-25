630aae74d9695ab90921dc2a5943c11f4b2308fb
Dilyana <d-yordanova@mail.bg>
2021-12-25 22:14:15 +0200

    Added solution.

---
+++ 16.ExamPreparationMay2019/01.FoodDelivery/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExamPreparationMay2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chicken = int.Parse(Console.ReadLine());
+            int fish = int.Parse(Console.ReadLine());
+            int vegetarian = int.Parse(Console.ReadLine());
+            double chichenPrice = 10.35;
+            double fishPrice = 12.40;
+            double vegetarianPrice = 8.15;
+            double food = chicken * chichenPrice + fish * fishPrice + vegetarian * vegetarianPrice;
+            double dessertPrice = food * 0.2;
+            double delivery = 2.50;
+            Console.WriteLine($"Total: {food + dessertPrice + delivery:f2}");
+
+        }
+    }
+}
+
