3548bf79bc6946430d81a8cd5cecb3edd9a1b3f9
Dilyana <d-yordanova@mail.bg>
2021-12-29 10:55:05 +0200

    Added solution.

---
+++ 16.ExamPreparationMay2019/04.TouristShop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _04.TouristShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            int countForAll = 0;
+            int countForThirtProduct = 0;
+            double totalPrice = 0;
+            string text = Console.ReadLine();
+            while (text != "Stop")
+            {
+                countForThirtProduct++;
+                double priceProduct = double.Parse(Console.ReadLine());
+                if (countForThirtProduct == 3)
+                {
+                    countForThirtProduct = 0;
+                    priceProduct = priceProduct / 2;
+                }
+
+                totalPrice += priceProduct;
+
+                if (totalPrice > budjet)
+                {
+                    Console.WriteLine($"You don't have enough money!");
+                    Console.WriteLine($"You need {totalPrice - budjet:f2} leva!");
+                    break;
+                }
+
+                countForAll++;
+                text = Console.ReadLine();
+            }
+
+            if (text == "Stop")
+            {
+                Console.WriteLine($"You bought {countForAll} products for {totalPrice:f2} leva.");
+            }
+        }
+    }
+}
