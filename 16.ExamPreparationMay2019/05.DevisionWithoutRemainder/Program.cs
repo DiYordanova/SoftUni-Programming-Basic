7b16c1b4126183b5917c595a8bc8c33355d79fb6
Dilyana <d-yordanova@mail.bg>
2021-12-30 12:10:01 +0200

    Added solution.

---
+++ 16.ExamPreparationMay2019/05.DevisionWithoutRemainder/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _05.DevisionWithoutRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int counter2 = 0;
+            int counter3 = 0;
+            int counter4 = 0;
+            for (int i = 0; i < number; i++)
+            {
+            int currentNumber = int.Parse(Console.ReadLine());
+                if (currentNumber % 2 == 0)
+                {
+                    counter2++;
+                }
+
+                if (currentNumber % 3 == 0)
+                {
+                    counter3++;
+                }
+
+                if (currentNumber % 4 == 0)
+                {
+                    counter4++;
+                }
+            }
+
+            double percentage2 = (double)counter2 / number * 100;
+            double percentage3 = (double)counter3 / number * 100;
+            double percentage4 = (double)counter4 / number * 100;
+
+            Console.WriteLine($"{percentage2:f2}%");
+            Console.WriteLine($"{percentage3:f2}%");
+            Console.WriteLine($"{percentage4:f2}%");
+        }
+    }
+}
