26e7b85772e47fb98c56fcee23fdbfdcdd91c5ad
Dilyana <d-yordanova@mail.bg>
2021-12-31 07:32:55 +0200

    Added solution.

---
+++ 16.ExamPreparationMay2019/06.VetParking/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06.VetParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            int hour = int.Parse(Console.ReadLine());
+            double totalSum = 0;
+            double price = 0;
+            int counter = 0;
+            for (int i = 1; i <= day; i++)
+            {
+
+                double daySum = 0;
+                for (int j = 1; j <= hour; j++)
+                {
+                    if (i % 2 == 0 && j % 2 == 1)
+                    {
+                        price = 2.50;
+                    }
+
+                    else if (i % 2 == 1 && j % 2 == 0)
+                    {
+                        price = 1.25;
+                    }
+
+                    else
+                    {
+                        price = 1;
+                    }
+                    daySum += price;
+                }
+                totalSum += daySum;
+                counter++;
+                Console.WriteLine($"Day: {counter} - {daySum:f2} leva");
+            }
+            Console.WriteLine($"Total: {totalSum:f2} leva");
+        }
+    }
+}
