327dfec3e30368dcfa1a6871b24302e73a57a145
Dilyana <d-yordanova@mail.bg>
2022-01-05 17:16:56 +0200

    Added solution.

---
+++ 17.ExamPreparationOctomber/01.MovieProfit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExamPreparationOctomber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int day = int.Parse(Console.ReadLine());
+            int ticket = int.Parse(Console.ReadLine());
+            double priceTicket = double.Parse(Console.ReadLine());
+            int percentage = int.Parse(Console.ReadLine());
+            double incomeTotal = ticket * priceTicket * day;
+            double incomeForCinema = percentage * incomeTotal / 100;
+            double profit = incomeTotal - incomeForCinema;
+            Console.WriteLine($"The profit from the movie {name} is {profit:f2} lv.");
+        }
+    }
+}
+
