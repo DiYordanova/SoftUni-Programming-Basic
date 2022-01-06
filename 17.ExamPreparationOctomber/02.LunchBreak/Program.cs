477418d9a80ea44c1534f03326eee9492a361971
Dilyana <d-yordanova@mail.bg>
2022-01-06 11:54:12 +0200

    Added solution.

---
+++ 17.ExamPreparationOctomber/02.LunchBreak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double durationEpisode = int.Parse(Console.ReadLine());
+            double durationRest = int.Parse(Console.ReadLine());
+            double timeForLunch = durationRest / 8;
+            double timeForRelaxation = durationRest / 4;
+            double timeRest = durationRest - timeForLunch - timeForRelaxation;
+            if (timeRest >= durationEpisode)
+            {
+                Console.WriteLine($"You have enough time to watch {name} and left with {Math.Ceiling(timeRest - durationEpisode)} minutes free time.");
+            }
+
+            else
+            {
+
+                Console.WriteLine($"You don't have enough time to watch {name}, you need {Math.Ceiling(durationEpisode - timeRest)} more minutes.");
+            }
+        }
+    }
+}
