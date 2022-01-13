2237fe028fa9575953545acf6785d00ec14205cf
Dilyana <d-yordanova@mail.bg>
2022-01-13 09:31:59 +0200

    Added solution.

---
+++ 18.ExamPreparationMarch2020/03.CareOfPuppy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05.CareOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int food = int.Parse(Console.ReadLine());
+            int foodInGram = food * 1000;
+            string input = Console.ReadLine();
+            double totalFood = 0;
+            while (input != "Adopted")
+            {
+                int currentFood = int.Parse(input);
+                totalFood += currentFood;
+                input = Console.ReadLine();
+            }
+
+            if (foodInGram >= totalFood)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodInGram - totalFood} grams.");
+            }
+
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {totalFood - foodInGram} grams more.");
+            }
+        }
+    }
+}
+
