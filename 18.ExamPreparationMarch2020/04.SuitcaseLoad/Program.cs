83eddc0f71db5da03c1a554e92a25c15f5fa64fd
Dilyana <d-yordanova@mail.bg>
2022-01-13 09:32:42 +0200

    Added solution.

---
+++ 18.ExamPreparationMarch2020/04.SuitcaseLoad/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05.SuitcaseLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double trunkCapacity = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int counter = 0;
+            while (input != "End")
+            {
+                double volumeSuitcase = double.Parse(input);
+                counter++;
+                if (counter % 3 == 0)
+                {
+                    volumeSuitcase = volumeSuitcase + (volumeSuitcase * 0.10);
+                }
+                if (trunkCapacity <= volumeSuitcase)
+                {
+                    break;
+                }
+                trunkCapacity -= volumeSuitcase;
+                input = Console.ReadLine();
+            }
+
+            if (input == "End")
+            {
+                Console.WriteLine($"Congratulations! All suitcases are loaded!");
+                Console.WriteLine($"Statistic: {counter} suitcases loaded.");
+            }
+
+            else
+            {
+                Console.WriteLine($"No more space!");
+                Console.WriteLine($"Statistic: {counter - 1} suitcases loaded."); ;
+            }
+        }
+    }
+}
+
