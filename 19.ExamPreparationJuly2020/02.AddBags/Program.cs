509fe58c3ca6f9755ae9cd701377f9851d425d76
Dilyana <d-yordanova@mail.bg>
2022-01-17 10:26:09 +0200

    Added solution.

---
+++ 19.ExamPreparationJuly2020/02.AddBags/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02.AddBags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceBaggageUnder20kg = double.Parse(Console.ReadLine());
+            double kgBaggage = double.Parse(Console.ReadLine());
+            int daysBeforeTravel = int.Parse(Console.ReadLine());
+            int countBaggage = int.Parse(Console.ReadLine());
+            double price = 0;
+            if (kgBaggage < 10)
+            {
+                price = priceBaggageUnder20kg * 0.2;
+            }
+
+            else if (kgBaggage <= 20)
+            {
+                price = priceBaggageUnder20kg * 0.5;
+            }
+
+            else if (kgBaggage > 20)
+            {
+                price = priceBaggageUnder20kg;
+            }
+            if (daysBeforeTravel < 7)
+            {
+                price += price * 0.40;
+            }
+
+            else if (daysBeforeTravel <= 30)
+            {
+                price += price * 0.15;
+            }
+
+            else if (daysBeforeTravel > 30)
+            {
+                price += price * 0.10;
+            }
+
+            Console.WriteLine($"The total price of bags is: {price * countBaggage:f2} lv. ");
+        }
+    }
+}
