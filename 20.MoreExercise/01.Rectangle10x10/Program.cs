18ccb87697adf9e749443dacef80edbc8ea1b51f
Dilyana <d-yordanova@mail.bg>
2022-01-19 00:53:13 +0200

    Added solution.

---
+++ 20.MoreExercise/01.Rectangle10x10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace moreExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = '*';
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write($"{symbol}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
