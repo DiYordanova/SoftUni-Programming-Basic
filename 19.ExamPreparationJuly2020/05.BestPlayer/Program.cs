610e6e293bf1ae850e1d8bc6911a2418e7b9382d
Dilyana <d-yordanova@mail.bg>
2022-01-19 00:49:25 +0200

    Added solution.

---
+++ 19.ExamPreparationJuly2020/05.BestPlayer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05.BestPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int maxGoal = int.MinValue;
+            string nameMaxGoal = "";
+            while (name != "END")
+            {
+                int goal = int.Parse(Console.ReadLine());
+                if (goal > maxGoal)
+                {
+                    maxGoal = goal;
+                    nameMaxGoal = name;
+                }
+
+                if (goal >= 10)
+                {
+                    Console.WriteLine($"{nameMaxGoal} is the best player!");
+                    Console.WriteLine($"He has scored {maxGoal} goals and made a hat-trick !!!");
+                    return;
+                }
+                name = Console.ReadLine();
+            }
+
+            if (name == "END")
+            {
+                Console.WriteLine($"{nameMaxGoal} is the best player!");
+            }
+
+            if (maxGoal >= 3)
+            {
+                Console.WriteLine($"He has scored {maxGoal} goals and made a hat-trick !!!");
+            }
+
+            else
+            {
+                Console.WriteLine($"He has scored {maxGoal} goals.");
+            }
+        }
+    }
+}
