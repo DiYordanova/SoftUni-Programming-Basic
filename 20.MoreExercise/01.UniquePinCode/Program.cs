d372a12b845257678c5213dea811d7dc27634858
Dilyana <d-yordanova@mail.bg>
2022-01-20 18:15:32 +0200

    Added solution.

---
+++ 20.MoreExercise/01.UniquePinCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _03.sqyareOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= a; i++)
+            {
+                for (int j = 1; j <= b; j++)
+                {
+                    for (int k = 1; k <= c; k++)
+                    {
+                        if (i % 2 == 0 && k % 2 == 0 && j >= 2 && j <= 7 && j != 4 && j != 6)
+                        {
+                            Console.WriteLine($"{i} {j} {k}");
+                        }
+
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
