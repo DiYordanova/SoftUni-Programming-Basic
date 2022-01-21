59a204fa32631598a55d83005d8895fbaec7bc30
Dilyana <d-yordanova@mail.bg>
2022-01-21 08:35:13 +0200

    Added solution.

---
+++ 20.MoreExercise/02.RectangleNxN/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.RectangleNxN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            char symbol = '*';
+            for (int i = 0; i < num; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write($"{symbol}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
