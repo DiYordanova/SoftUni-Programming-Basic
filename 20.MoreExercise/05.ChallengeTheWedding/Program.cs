8c9580040d7806aca73d0311dd62d21d0018f9e7
Dilyana <d-yordanova@mail.bg>
2022-01-22 18:50:40 +0200

    Added solution.

---
+++ 20.MoreExercise/05.ChallengeTheWedding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05.ChallengeTheWedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int f = int.Parse(Console.ReadLine());
+            int table = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= f; j++)
+                {
+                    if (table > (i + j))
+                    {
+                        Console.Write($"({i} <-> {j}) ");
+                    }
+
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
